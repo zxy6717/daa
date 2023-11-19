aeb177289d4dcf2bbffaf9e68fb6a443346d7ea0
zxy6717 <151444069+zxy6717@users.noreply.github.com>
2023-11-19 18:34:01 +0530

    Add files via upload

---
+++ cpu.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    // Matrix for storing Process Id, Burst
+    // Time, Average Waiting Time & Average
+    // Turn Around Time.
+    int A[100][4];
+    int i, j, n, total = 0, index, temp;
+    float avg_wt, avg_tat;
+    
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    
+    printf("Enter Burst Time:\n");
+    // User Input Burst Time and allocating Process Id.
+    for (i = 0; i < n; i++) {
+        printf("P%d: ", i + 1);
+        scanf("%d", &A[i][1]);
+        A[i][0] = i + 1;
+    }
+    
+    // Sorting processes according to their Burst Time.
+    for (i = 0; i < n; i++) {
+        index = i;
+        for (j = i + 1; j < n; j++)
+            if (A[j][1] < A[index][1])
+                index = j;
+        temp = A[i][1];
+        A[i][1] = A[index][1];
+        A[index][1] = temp;
+        temp = A[i][0];
+        A[i][0] = A[index][0];
+        A[index][0] = temp;
+    }
+    
+    A[0][2] = 0;
+    // Calculation of Waiting Times
+    for (i = 1; i < n; i++) {
+        A[i][2] = 0;
+        for (j = 0; j < i; j++)
+            A[i][2] += A[j][1];
+        total += A[i][2];
+    }
+    
+    avg_wt = (float)total / n;
+    total = 0;
+    
+    printf("P BT WT TAT\n");
+    // Calculation of Turn Around Time and printing the
+    // data.
+    for (i = 0; i < n; i++) {
+        A[i][3] = A[i][1] + A[i][2];
+        total += A[i][3];
+        printf("P%d %d %d %d\n", A[i][0],
+               A[i][1], A[i][2], A[i][3]);
+    }
+    
+    avg_tat = (float)total / n;
+    printf("Average Waiting Time= %f\n", avg_wt);
+    printf("Average Turnaround Time= %f\n", avg_tat);
+    
+    return 0;
+}
